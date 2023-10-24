46529a731688dbd18b0da0d312049e98a2ac7b35
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-10-24 11:47:12 +0800

    Add manager controller for client

---
+++ Client/Controllers/ManagerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+public class ManagerController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Auth
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        return View();
+    }
+
+    // Leaves Management
+    public IActionResult ManageLeaveApplications()
+    {
+        return View();
+    }
+
+    public IActionResult ManageLeaveHistories()
+    {
+        return View();
+    }
+
+    
+    // Employeee Management 
+    public IActionResult GetAllEmployee()
+    {
+        return View();
+    }
+
+    public IActionResult GetEmployeeDetailByNik()
+    {
+        return View();
+    }
+
+    public IActionResult CreateEmployee()
+    {
+        return View();
+    }
+
+    
+}
