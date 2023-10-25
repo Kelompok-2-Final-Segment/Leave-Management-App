d0c1b136f5ee51796009f24df7affb5991a0087e
farhanzuhri <101404632+farhanzuhri@users.noreply.github.com>
2023-10-25 10:12:09 +0700

    Update

---
+++ Client/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+public class EmployeeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Auth
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        return View();
+    }
+
+    // Leaves Management
+    public IActionResult RequestLeave()
+    {
+        return View();
+    }
+
+    public IActionResult SeeOnRequestLeave()
+    {
+        return View();
+    }
+
+    public IActionResult ManageLeaveHistories()
+    {
+        return View();
+    }
+
+}
