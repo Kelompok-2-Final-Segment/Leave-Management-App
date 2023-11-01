3ae9a5ea01fa5cb253b7b203a42203799d1e23e4
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-11-01 17:29:09 +0800

    Update admin dash auth and desain

--- Client/Controllers/AdminController.cs
+++ Client/Controllers/AdminController.cs
@@ -14,6 +14,7 @@ using System.Diagnostics;
 
 namespace Client.Controllers;
 
+[Authorize(Policy = "Admin")]
 public class AdminController : Controller
 {
     private readonly IAdminRepository adminRepository;
