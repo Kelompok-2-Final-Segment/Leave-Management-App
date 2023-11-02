69bbfcf931ef9aba49b9fd4fe45e0fac99ecc0e4
fanalis90 <33200884+fanalis90@users.noreply.github.com>
2023-11-02 15:35:15 +0700

    push terahir

--- Client/wwwroot/js/staff/cancelLeave.js
+++ Client/wwwroot/js/staff/cancelLeave.js
@@ -17,24 +17,26 @@ $(document).ready(function () {
                     "GuidLeave": leaveId,
                     "GuidEmployee": employeeId
                 }
-
-                $.post("@Url.Action("CancelLeave")", values, function (data) {
-                    // do stuff;
-                }).done(() => {
-                    Swal.fire(
-                        'Cancelled!',
-                        'Your leave has been cancelled.',
-                        'success'
-                    )
-                }).fail((err) => {
-                    Swal.fire(
-                        'Fail!',
-                        'sorry you can try again later',
-                        'error'
-                    )
-                }) ;
-
+                $.ajax({
+                    url: "/leave/edit/" + leaveId,
+                    method: 'POST',
+                    dataType: 'json'
+                })
+                    .done((result) => {
+                        Swal.fire(
+                            'Cancelled!',
+                            'Your leave has been cancelled.',
+                            'success'
+                        )
+                        window.location.href = urlaction;
                
+                    }).fail((err) => {
+                        Swal.fire(
+                            'Fail!',
+                            'sorry you can try again later',
+                            'error'
+                        )
+                    });
 
 
             }
